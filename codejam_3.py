61caa7c7454d782f83af41dc703c31f3cf1777ff
maheensaleh <salehmaheen@gmail.com>
2020-04-03 12:40:00 +0500

    getting tle for prob 3

---
+++ codejam_3.py
@@ -0,0 +1,85 @@
+T = int(input())
+for t in range(T):
+    n,l= map(int, input().split())
+    prods=[int(x) for x in input().split()]
+    # n,l = 10000 , 25
+    # prods = [217 ,1891 ,4819, 2291, 2987, 3811, 1739, 2491, 4717, 445, 65 ,1079, 8383, 5353 ,901,187,649,1003,697,3239,7663,291,123,779,1007,3551,1943,2117,1679,989,3053]
+    # print(prods)
+
+
+    primes = []
+    apls = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    alphas = apls.split(" ")
+
+    p=2
+    primes.append(p)
+
+    while p<n:
+      p+=1
+      ok = True
+      for i in range(2,p//2):
+        rem = p%i
+        if rem==0:
+          ok = False
+          break
+        else:
+          continue
+      if ok:
+        primes.append(p)
+    # primes = sorted(primes)
+    # print(primes)
+    # print(alphas)
+    # print(len(primes))
+    # print(len(primes)==len(alphas))
+
+    # get the factors of prods:
+    facs=[]
+    for i in prods:
+      done = False
+      c=0
+      while not done:
+        rem = i%primes[c]
+        if rem ==0:
+          facs.append((primes[c],int(i/primes[c])))
+          done  = True
+        c+=1
+
+    # print(facs)
+    prime_code=[]
+    codes =[]
+
+    for i in facs[0]:
+      if i not in facs[1]:
+        prime_code.append(i)
+        codes.append(i)
+
+    for i in range(len(facs)-1):
+      for j in facs[i]:
+        if j in facs[i+1]:
+          prime_code.append(j)
+          if j not in codes:
+            codes.append(j)
+    # print(prime_code)
+    # print(len(prime_code))
+
+    for i in facs[-1]:
+      if i not in facs[-2]:
+        prime_code.append(i)
+        if i not in codes:
+          codes.append(i)
+    codes = sorted(codes)
+    ans = ""
+    # print(prime_code,codes)
+    for i in prime_code:
+      # print(i)
+      ans=ans+alphas[codes.index(i)]
+    # print(ans,len(ans))
+    print("Case #{}: {} ".format(t + 1, ans.upper()))
+
+
+
+
+
+
+
+
